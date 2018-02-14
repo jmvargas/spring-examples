67be4b94acb52f4439f14e506a41594a8d70630f
Jesús Manuel Vargas <jesusmanuel.vargas@ext.privalia.com>
2018-02-14 15:22:16 +0100

    Added aop with annotations (fixed)

--- src/main/java/com/privalia/aspectos/annotations/Compra.java
+++ src/main/java/com/privalia/aspectos/annotations/Compra.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Compra {
-
     public void compra(boolean error) throws Exception {
         if (error) {
             throw new Exception("Algo ha ido mal");
--- src/main/java/com/privalia/aspectos/annotations/ValidarMaterial.java
+++ src/main/java/com/privalia/aspectos/annotations/ValidarMaterial.java
@@ -4,22 +4,25 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass=true)
 @Aspect
 public class ValidarMaterial {
-    @Before("execution( * com.privalia.aspectos.annotations.Compra.compra(..) )")
+    @Before("execution(* com.privalia.aspectos.annotations.Compra.compra(..))")
     public void comprobarCantidad(){
         System.out.println("Se comprueba que la cantidad es correcta");
     }
 
-    @After("execution( * com.privalia.aspectos.annotations.Compra.compra(..) )")
+    @After("execution(* com.privalia.aspectos.annotations.Compra.compra(..))")
     public void empaquetar(){
         System.out.println("Se empaqueta la mercancia");
     }
 
-    @AfterThrowing("execution( * com.privalia.aspectos.annotations.Compra.compra(..) )")
+    @AfterThrowing("execution(* com.privalia.aspectos.annotations.Compra.compra(..))")
     public void ponerReclamacion(){
         System.out.println("Se reclaman los defectos detectados");
     }
